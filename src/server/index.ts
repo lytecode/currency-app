03908fb554973e1729d40f5a2adfd4697582b3b7
Mbonu Basil <mbonubasil@gmail.com>
2022-12-13 14:28:15 +0100

    Initialize app and add a server

---
+++ src/server/index.ts
@@ -0,0 +1,8 @@
+import app from '../app';
+import http from 'http';
+import config from '../config';
+
+const port = config.PORT || 5000;
+app.set('port', port);
+
+const server = http.createServer(app);
